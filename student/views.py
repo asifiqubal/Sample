de5a24fa296f07393ebb0cadbf93418486b524d1
asifiqubal <asif_iqubal@outlook.com>
2017-03-09 05:10:59 +0600

    mysql Db connection success

---
+++ student/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
+
+def index(request):
+    template=loader.get_template('index.html')
+    context={
+        'department':Department.objects.all()
+
+    }
+    return HttpResponse(template.render(context,request))
+
+def student(request):
+    template=loader.get_template('student.html')
+    context={
+
+    }
+    return HttpResponse(template.render(context,request))
