8bdedf4867ac609b46c5b32f9ca653725644329f
asifiqubal <asif_iqubal@outlook.com>
2017-03-18 01:34:30 +0600

    remove hardcoded link and show does not exist error

--- Sample/urls.py
+++ Sample/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^',include('student.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('student.urls')),
+    url(r'^login/', include('login.urls')),
+    url(r'^dbadmin/', admin.site.urls),
 ]
--- student/views.py
+++ student/views.py
@@ -1,21 +1,18 @@
-
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render,get_object_or_404
 
 from .models import *
 
 def index(request):
 
-    template = loader.get_template('index.html')
     context = {
         'department': Department.objects.all()
 
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
-def student(request, sid):
-    template = loader.get_template('student.html')
+def student( request, sid):
     context = {
-        'student': Student.objects.get(id=sid)
+    'student': get_object_or_404(Student, pk=sid)
     }
-    return HttpResponse(template.render(context, request))
+
+    return render(request, "student.html", context)
